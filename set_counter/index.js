7bf91dac8d10f92589dce88baaa20da9d1cdf6a1
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2023-02-10 16:18:40 -0500

    save feature

--- set_counter/index.js
+++ set_counter/index.js
@@ -1,3 +1,4 @@
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
@@ -6,3 +7,9 @@ function increment() {
     countEl.textContent = count
 }
 
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
