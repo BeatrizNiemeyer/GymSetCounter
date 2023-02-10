10473dc61c3b389b3c75f15eb4470815918d9f7e
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2023-02-10 17:33:12 -0500

    exercise's name feature

--- set_counter/index.js
+++ set_counter/index.js
@@ -2,14 +2,18 @@ let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
+
 function increment() {
     count += 1
-    countEl.textContent = count
+    countEl.innerHTML = count
 }
 
 function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
-    countEl.textContent = 0
+    let exerciseName = document.getElementById("exercise-name").value
+    let countStr = exerciseName + ": " + count + " - "
+    saveEl.innerHTML += countStr
+    countEl.innerHTML = 0
     count = 0
+    exerciseName= ""
+    
 }
